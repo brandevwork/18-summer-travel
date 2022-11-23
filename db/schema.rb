49ab4c1922e90055d13c695cb99f4bf2f9a0c702
joseph jennings <joseph.jennings@gmail.com>
2022-11-23 13:57:59 +0500

    Add family member migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_090418) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_083458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_090418) do
     t.index ["email"], name: "index_families_on_email", unique: true
     t.index ["jti"], name: "index_families_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_families_on_reset_password_token", unique: true
+  end
+
+  create_table "family_members", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.date "date_of_birth", null: false
+    t.bigint "family_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["family_id"], name: "index_family_members_on_family_id"
   end
 
 end

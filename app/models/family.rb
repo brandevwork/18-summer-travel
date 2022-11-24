8c6472d7cf65e8c781a38bc7b354e8ae7fbfb254
joseph jennings <joseph.jennings@gmail.com>
2022-11-24 13:24:03 +0500

    Family Member: Added age callback and presence validations in familymember model

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,7 +2,7 @@ class Family < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   validates :first_name, presence: true
   validates :number_of_family_member, presence: true, numericality: { less_than_or_equal_to: 6 }
-  validates :address, :city, :country, presence: true
+  validates :address, :city, :country, :state, presence: true
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
--- app/models/family_member.rb
+++ app/models/family_member.rb
@@ -1,3 +1,11 @@
 class FamilyMember < ApplicationRecord
-    belongs_to :family
+  belongs_to :family
+  before_save :set_age
+  validates :name, :date_of_birth, :family_id, presence: true
+
+  private
+
+  def set_age
+    self.age = Time.zone.now.year - date_of_birth.year
+  end
 end

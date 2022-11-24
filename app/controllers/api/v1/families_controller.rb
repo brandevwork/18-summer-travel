05f7d16e49713ac2a34eadb328554b299acc5be9
joseph jennings <joseph.jennings@gmail.com>
2022-11-24 12:33:11 +0500

    Login and log out token create for family

--- app/controllers/api/v1/families_controller.rb
+++ app/controllers/api/v1/families_controller.rb
@@ -1,5 +1,6 @@
 class  Api::V1::FamiliesController < ApplicationController
   before_action :authenticate_family!
+
   def index
     render json: families, status: :ok
   end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module SurveyTool
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end

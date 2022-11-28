9afae3ab62ff30136d327d63eec869c7ca15d4ce
joseph jennings <joseph.jennings@gmail.com>
2022-11-28 12:58:15 +0500

    Survey: Added function to set survey_type on depends on the age of the member

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,11 @@ class Survey < ApplicationRecord
   enum status: [:not_started, :started, :completed]
   enum survey_type: [:adult, :kids]
   belongs_to :family_member
+  after_create :set_survey_type
+
+  private
+
+  def set_survey_type
+    update(survey_type: (family_member.age > 14)? 0 : 1)
+  end
 end

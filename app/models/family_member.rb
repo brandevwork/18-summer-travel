7be38760422b6321a33734378c1ac1c4fb0268a2
joseph jennings <joseph.jennings@gmail.com>
2022-12-05 15:40:52 +0500

    Family Memeber: Remove Survey type from family member model

--- app/models/family_member.rb
+++ app/models/family_member.rb
@@ -1,8 +1,6 @@
 class FamilyMember < ApplicationRecord
   enum survey_status: [:not_started, :started, :completed]
-  enum member_type: [:adult, :kids]
   before_save :set_age
-  after_commit :set_member_status, on: :create
 
   belongs_to :family
 
@@ -19,10 +17,6 @@ class FamilyMember < ApplicationRecord
     if date_of_birth.present? && date_of_birth.year > Time.zone.now.year
         errors.add(:date_of_birth, 'Date of birth must be less than today.')
     end
-  end
-
-  def set_member_status
-    update(survey_type: (family_member.age > 14)? 0 : 1)
   end
 
 end

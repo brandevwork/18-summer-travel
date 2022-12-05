1bee2ebf1e417e4bb1bad0c5b877683a43268af0
joseph jennings <joseph.jennings@gmail.com>
2022-12-05 15:45:14 +0500

    Survey: Added survey, question and choices for adults in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+
+questions = JSON.parse(File.read('question_choices.json'))
+survey = Survey.find_or_create_by(survey_name: "18 Summer Tool", survey_type: "adult")
+
+questions.each do |question|
+  q=Question.find_or_create_by(question_text: question["question"], survey_id: survey.id)
+  question["choices"].each do |choice|
+    Choice.find_or_create_by(choice_text: choice, question_id: q.id)
+  end
+end

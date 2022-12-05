224e949fa7e813c9270d8b5d05fb59a40e7c94ea
joseph jennings <joseph.jennings@gmail.com>
2022-12-05 12:20:44 +0500

    Family Member: Added survey_status, member_type fields in family member model

--- db/migrate/20221123083458_create_family_member.rb
+++ db/migrate/20221123083458_create_family_member.rb
@@ -6,6 +6,8 @@ class CreateFamilyMember < ActiveRecord::Migration[7.0]
       t.date :date_of_birth,          null: false
       t.boolean :status, default: true
       t.references :family
+      t.integer :survey_status, default: 0
+      t.integer :member_type
       t.timestamps
     end
   end

fbb1f39ab05516d53560c0e22c9edbb38ce040ab
joseph jennings <joseph.jennings@gmail.com>
2022-12-06 17:55:18 +0500

    Response: Added despendent destroy

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,6 @@
 class Response < ApplicationRecord
   belongs_to :family_member
-  has_many :response_choices
+  has_many :response_choices, dependent: :destroy
   has_many :choices, through: :response_choices
   accepts_nested_attributes_for :response_choices
 end

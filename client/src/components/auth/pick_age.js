ca2ffb971387d944d1e7c5d1bbdde3adb0cbb771
joseph jennings <joseph.jennings@gmail.com>
2022-12-06 16:24:31 +0500

    Added refs arrays to name and years

--- client/src/components/auth/pick_age.js
+++ client/src/components/auth/pick_age.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React,{ useRef } from 'react';
 import Button from '../UI/Button';
 import Input from '../UI/Input';
 
 
 function PickAge(props)	{
+	const nameRef =  useRef([])
+	const yearRef =  useRef([])
+
+	let memberData=[]
 	let totalMembers = props.familyMemberState.family_member
 	const renderNameAndYear = () => {
 		let content = [];
 		for(let i = 0; i < totalMembers-1; i++) {
 			content.push(
 				<div style={{"display":"flex"}}>
-			    <Input  input={{"type":"text", "placeholder":"Name",
+			    <Input ref={el => nameRef.current[i+1] = el}  input={{"key":Math.random() , "type":"text", "placeholder":"Name",
 			      "className":"form-control rounded input-lg text-center no-border"}}/>
-			    <Input  input={{"type":"text", "placeholder":"Year",
+			    <Input ref={el => yearRef.current[i+1] = el} input={{"key":Math.random() ,"type":"text", "placeholder":"Year",
 			      "className":"form-control rounded input-lg text-center no-border"}}/>
 			  </div> 
 			)
@@ -20,17 +24,23 @@ function PickAge(props)	{
 		return content   
 	}
 
+	const nextHandler = (e) => {
+		console.log(nameRef.current)
+		console.log(yearRef.current)
+		// props.nextClickHandler('confirm_age',{"family_members":memberData})
+	}
+
 	return (
 		<div>
   		<p>Age</p>
   		<div style={{"display":"flex"}}>
-  		<Input  input={{"type":"text", "value": [props.familyMemberState.first_name], "placeholder":"Name",
+  		<Input ref={el => nameRef.current[0] = el}  input={{"type":"text", "value": [props.familyMemberState.first_name], "placeholder":"Name",
 			      "className":"form-control rounded input-lg text-center no-border"}}/>
-	    <Input  input={{"type":"text", "placeholder":"Year",
+	    <Input ref={el => yearRef.current[0] = el}  input={{"type":"text", "placeholder":"Year",
 	      "className":"form-control rounded input-lg text-center no-border"}}/>
 	      </div>
   		{renderNameAndYear ()}
-  		<Button title="Next" buttonClickHandler={() => props.nextClickHandler('confirm_age')}/>
+  		<Button title="Next" buttonClickHandler={nextHandler}/>
   	</div>
 	)
 }

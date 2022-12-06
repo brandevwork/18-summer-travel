98c527f6a29e9441b0b95f1e6e3f8d06b09539f3
joseph jennings <joseph.jennings@gmail.com>
2022-12-06 19:33:38 +0500

    Response: added accepts nested atrributes in model

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,5 @@ class Response < ApplicationRecord
   belongs_to :family_member
   has_many :response_choices
   has_many :choices, through: :response_choices
+  accepts_nested_attributes_for :response_choices
 end

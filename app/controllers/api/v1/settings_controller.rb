7d9c52b227d90d2c0977153882b5bbb5dd0de3f4
joseph jennings <joseph.jennings@gmail.com>
2022-12-07 13:39:54 +0500

    Settings: Added settings api swagger documentation

--- app/controllers/api/v1/settings_controller.rb
+++ app/controllers/api/v1/settings_controller.rb
@@ -1,10 +1,27 @@
 class Api::V1::SettingsController < ApplicationController
   include Api::V1
+
   skip_before_action :verify_authenticity_token
+
+  swagger_controller :settings, "Settings"
+
+  swagger_api :family do
+    summary "Fetches a family and its all family members on settings page"
+    response :unauthorized
+    response :not_acceptable, "The request you made is not acceptable"
+  end
+
+  swagger_api :reset_family_survey do
+    summary "Reset the survey response of all the family members of the family that is login"
+    response :unauthorized
+    response :not_acceptable, "The request you made is not acceptable"
+  end
+
 
   def family
     render json: current_family, serializer: FamiliesController::FamilySerializer, meta: {status: :ok, code: 200}
   end
+
   def reset_family_survey
     familymembers = current_family.family_members
     if familymembers.present?
@@ -16,4 +33,5 @@ class Api::V1::SettingsController < ApplicationController
       render json: { message: "Unable to reset survey", status: 204}
     end
   end
+
 end

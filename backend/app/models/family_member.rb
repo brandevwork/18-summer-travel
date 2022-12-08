d14bee908837efd87b2f5d38b7805ec6f7649057
joseph jennings <joseph.jennings@gmail.com>
2022-12-08 20:37:21 +0500

    FamilyMember: Update the birth_year callbacks

--- backend/app/models/family_member.rb
+++ backend/app/models/family_member.rb
@@ -5,18 +5,18 @@ class FamilyMember < ApplicationRecord
   belongs_to :family
   has_one :response, dependent: :destroy
 
-  validates :name, :date_of_birth, presence: true
-  validate :check_date_of_birth
+  validates :name, :birth_year, presence: true
+  validate :check_year_of_birth
 
   private
 
   def set_age
-    self.age = Time.zone.now.year - date_of_birth.year
+    self.age = Time.zone.now.year - birth_year.to_i
   end
 
-  def check_date_of_birth
-    if date_of_birth.present? && date_of_birth.year > Time.zone.now.year
-      errors.add(:date_of_birth, 'Date of birth must be less than today.')
+  def check_year_of_birth
+    if birth_year.present? && birth_year.to_i > Time.zone.now.year
+      errors.add(:birth_year, 'Birth year must be less than current year.')
     end
   end
 

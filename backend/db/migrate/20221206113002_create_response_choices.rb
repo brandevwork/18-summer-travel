ced9d3cff3261e25ca090a0c688d4fd50b68e3d4
joseph jennings <joseph.jennings@gamil.com>
2022-12-08 20:40:05 +0500

    ResponseChoice: Add the score attribute in response choice

--- backend/db/migrate/20221206113002_create_response_choices.rb
+++ backend/db/migrate/20221206113002_create_response_choices.rb
@@ -1,6 +1,7 @@
 class CreateResponseChoices < ActiveRecord::Migration[7.0]
   def change
     create_table :response_choices do |t|
+      t.integer :score
       t.references :response, null: false, foreign_key: true
       t.references :choice, null: false, foreign_key: true
       t.timestamps

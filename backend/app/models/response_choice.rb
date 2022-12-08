b65a41714106e09cbb258cb6b1cd3d564d01b5d6
joseph jennings <joseph.jennings@gamil.com>
2022-12-08 20:39:12 +0500

    ResponseChoice: Add the function to add the score

--- backend/app/models/response_choice.rb
+++ backend/app/models/response_choice.rb
@@ -1,4 +1,11 @@
 class ResponseChoice < ApplicationRecord
+  after_commit :choice_score
   belongs_to :response
   belongs_to :choice
+
+  def choice_score
+    update_column(:score, 1)
+  end
+
+
 end

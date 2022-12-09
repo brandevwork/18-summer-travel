7dea61acd47aa55ca8da59e9ffb48bc3d06ec9b8
joseph jennings <joseph.jennings@gmail.com>
2022-12-09 14:33:06 +0500

    FamilyMember: Updated status of family-member when a response is created

--- backend/app/models/response.rb
+++ backend/app/models/response.rb
@@ -1,6 +1,12 @@
 class Response < ApplicationRecord
+  after_commit :update_family_member_status, on: :create
   belongs_to :family_member
   has_many :response_choices, dependent: :destroy
   has_many :choices, through: :response_choices
   accepts_nested_attributes_for :response_choices
+
+  private
+  def update_family_member_status
+    family_member.update(survey_status: 1)
+  end
 end

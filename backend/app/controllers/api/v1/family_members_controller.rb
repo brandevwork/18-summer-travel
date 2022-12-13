ba539ae32054a41ee6c610c2418db615d46879b6
joseph jennings <joseph.jennings@gmail.com>
2022-12-13 14:52:45 +0500

    Swagger: Updated the swagger-documentation for family-member controller

--- backend/app/controllers/api/v1/family_members_controller.rb
+++ backend/app/controllers/api/v1/family_members_controller.rb
@@ -7,21 +7,22 @@ class  Api::V1::FamilyMembersController < BaseController
   swagger_api :index do
     summary "Fetches all family members of a family"
     notes "These are the family members against a family"
-    param :query, :family_id, :integer, :optional, "Family Id"
     response :unauthorized
     response :not_acceptable, "The request you made is not acceptable"
   end
 
-  swagger_api :create do
-    summary "Creates a new Family member"
-    param :form, :name, :string, :required, "Name"
-    param :form, :birth_year, :date, :required, "Date of Birth"
-    param :form, :email, :string, :required, "Email address"
-    response :not_acceptable
-  end
 
   swagger_api :show do
     summary "Fetches a single family member and its survey"
+    param :path, :id, :integer, :required, "FamilyMember Id"
+    response :ok, "Success", :Survey
+    response :unauthorized
+    response :not_acceptable
+    response :not_found
+  end
+
+  swagger_api :update do
+    summary "Updates the is_active status of given id family member"
     param :path, :id, :integer, :required, "FamilyMember Id"
     response :ok, "Success", :Survey
     response :unauthorized
@@ -41,15 +42,6 @@ class  Api::V1::FamilyMembersController < BaseController
       render json: questions, each_serializer: QuestionsSerializer, meta: {status: :ok, code: 200}
     else
       render json: {error: "Family Member not found"}
-    end
-  end
-
-  def create
-    family_member = current_family.family_members.create(family_member_params)
-    if family_member.save
-      render json: family_member, status: 200
-    else
-      render json: {error: "Cannot create family member"}
     end
   end
 

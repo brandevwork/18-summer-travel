978d44b0062274ac93f0c00776b26da033b34d83
joseph jennings <joseph.jennings@gmail.com>
2022-12-15 18:34:27 +0500

    ResponseChoice: Updated the survey_status function for kids

--- backend/app/models/response_choice.rb
+++ backend/app/models/response_choice.rb
@@ -8,6 +8,10 @@ class ResponseChoice < ApplicationRecord
 
   def family_member_status
     family_member.update(survey_status: 1) if family_member.response_choices.count < 2
-    family_member.update(survey_status: 2) if family_member.response_choices.select(:question_id).distinct.count == Question.count
+    if family_member.age < 14
+      family_member.update(survey_status: 2) if family_member.response_choices.select(:question_id).distinct.count == Question.count - 12
+    else
+      family_member.update(survey_status: 2) if family_member.response_choices.select(:question_id).distinct.count == Question.count
+    end
   end
 end

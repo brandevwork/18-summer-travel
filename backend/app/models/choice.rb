c16de4cf80e1f388b836765a34c2cf274b6e5470
joseph jennings <joseph.jennings@gmail.com>
2022-12-15 19:27:55 +0500

    Choice: Added dependent destroy to response choices

--- backend/app/models/choice.rb
+++ backend/app/models/choice.rb
@@ -1,7 +1,7 @@
 class Choice < ApplicationRecord
   belongs_to :question
   belongs_to :category
-  has_many :response_choices
+  has_many :response_choices, dependent: :destroy
   has_many :family_members, through: :response_choices
   has_one_attached :image
 end

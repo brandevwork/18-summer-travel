945214565be57bcca07a9874c2199a5e037bcb55
joseph jennings <joseph.jennings@gmail.com>
2022-12-15 18:09:34 +0500

    Category: Develop the association with choice

--- backend/app/models/choice.rb
+++ backend/app/models/choice.rb
@@ -1,5 +1,6 @@
 class Choice < ApplicationRecord
   belongs_to :question
+  belongs_to :category
   has_many :response_choices
   has_many :family_members, through: :response_choices
   has_one_attached :image

4de57e8b30a85339f51a1df99b4e0bae381a500d
joseph jennings <joseph.jennings@gmail.com>
2022-12-15 15:59:11 +0500

    Added callback to update the survey status on response creation

--- backend/app/models/response_choice.rb
+++ backend/app/models/response_choice.rb
@@ -1,4 +1,13 @@
 class ResponseChoice < ApplicationRecord
   belongs_to :choices, optional: true
   belongs_to :family_member
+
+  after_create :family_member_status
+
+  private
+
+  def family_member_status
+    return if family_member.response_choices.count > 2
+    family_member.update(survey_status: 1)
+  end
 end

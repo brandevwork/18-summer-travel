0c3e8de0dc11b4f6538d208b0b74cd7b663cf4f4
joseph jennings <joseph.jennings@gmail.com>
2022-12-15 14:11:38 +0500

    ResponseChoice: Updated the code

--- backend/app/controllers/api/v1/response_choices_controller.rb
+++ backend/app/controllers/api/v1/response_choices_controller.rb
@@ -2,17 +2,18 @@ class  Api::V1::ResponseChoicesController < BaseController
 
   def create
     if params["family_member_id"].present? and params["question_id"].present?
-      if params["choice_ids"].nil?
+      if params["choice_ids"].length < 1
         ResponseChoice.find_or_create_by(family_member_id: params["family_member_id"], question_id: params["question_id"])
       else
-        params["choice_ids"].each do |choiceid|
-          ResponseChoice.find_or_create_by(family_member_id: params["family_member_id"], choice_id: choiceid, question_id: params["question_id"])
+        params["choice_ids"].each do |choice_id|
+          ResponseChoice.find_or_create_by(family_member_id: params["family_member_id"], choice_id: choice_id, question_id: params["question_id"])
         end
       end
-      render json: { success: true, status: 200 }
+      render json: { status: 200 , success: true }
     else
+      return render json: { message: "Family Member and Question id is missing" , status: 400 , success: false } unless (params["question_id"].present? || params["family_member_id"].present?) 
       error_message = params["family_member_id"].present? ? 'question_id is missing' : 'family_member_id is missing'
-      render json: { message: error_message , success: false, status: 400 }
+      render json: { message: error_message , status: 400 , success: false }
     end
   end
 

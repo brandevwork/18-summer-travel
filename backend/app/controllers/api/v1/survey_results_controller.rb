7f8e2843bcff818d263738d45fe3741f31854f5a
joseph jennings <joseph.jennings@gmail.com>
2022-12-16 20:30:53 +0500

    Result: Create a function to display the survey result

--- backend/app/controllers/api/v1/survey_results_controller.rb
+++ backend/app/controllers/api/v1/survey_results_controller.rb
@@ -1,8 +1,11 @@
 class  Api::V1::SurveyResultsController < BaseController
+  include Api::V1::CategoriesController
 
   def result
-    if current_family.family_members.completed
-
+    categories = Category.all
+    family_members = current_family.family_members.where("age > ?", 4)
+    if family_members.completed.length == family_members.count
+      render json: categories, each_serializer: CategorySerializer, meta: { status: :ok, code: 200, success: true, data: family_members.response_choices  }
     else
       render json: { message: "Survey is not completed by the whole famliy", status: 204}
     end

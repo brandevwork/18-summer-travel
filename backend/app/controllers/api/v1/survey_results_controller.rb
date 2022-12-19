160e074a4be839cab242dea7086b39e0260f070e
joseph jennings <joseph.jennings@gmail.com>
2022-12-19 19:15:58 +0500

    Result: Upated the survey result api

--- backend/app/controllers/api/v1/survey_results_controller.rb
+++ backend/app/controllers/api/v1/survey_results_controller.rb
@@ -1,11 +1,11 @@
 class  Api::V1::SurveyResultsController < BaseController
-  include Api::V1::CategoriesController
+  include Api::V1::FamilyMemberController
 
   def result
     categories = Category.all
     family_members = current_family.family_members.where("age > ?", 4)
     if family_members.completed.length == family_members.count
-      render json: categories, each_serializer: CategorySerializer, meta: { status: :ok, code: 200, success: true, data: family_members.response_choices  }
+      render json: family_members, each_serializer: FamilyMemberSerializer, meta: { status: :ok, code: 200, success: true }
     else
       render json: { message: "Survey is not completed by the whole famliy", status: 204}
     end

fa2a950f6543c9311595849250f784c4841dd00e
joseph jennings <joseph.jennings@gmail.com>
2022-12-20 18:33:07 +0500

    Updated  pick Age and confirm Age screens with images urls, classes, buttons and event handlers with dynamic data showing and set validations on UI. Also implemented Back button InPage and inputs

--- client/src/components/auth/pickAge.js
+++ client/src/components/auth/pickAge.js
@@ -1,5 +1,6 @@
 import React,{ useRef, useEffect } from 'react';
 import Button from '../UI/button';
+import Back from '../UI/back';
 import Input from '../UI/input';
 
 function yearValidation(year) {
@@ -28,12 +29,20 @@ function PickAge({familyMemberState, nextClickHandler})	{
 		let content = [];
 		for(let i = 0; i < totalMembers-1; i++) {
 			content.push(
-				<div style={{"display":"flex"}}>
-			    <Input ref={el => nameRef.current[i+1] = el}  input={{"key":Math.random() , "type":"text", "placeholder":"Name",
+				<li className="mb-3 ps-2">
+          <div className="list-grid">
+            <div className="cross-field">
+              <Input ref={el => nameRef.current[i+1] = el}  input={{"key":Math.random() , "type":"text", "placeholder":"Name",
 			      "className":"form-control rounded input-lg text-center no-border"}}/>
-			    <Input ref={el => yearRef.current[i+1] = el} input={{"key":Math.random() ,"type":"text", "placeholder":"Year",
+              <div className="cross-icon"></div>
+            </div>
+            <div className="cross-field">
+              <Input ref={el => yearRef.current[i+1] = el} input={{"key":Math.random() ,"type":"text", "placeholder":"Year",
 			      "className":"form-control rounded input-lg text-center no-border"}}/><span style={{"color":"red"}} ref={el => yearErrRef.current[i+1] = el}></span>
-			  </div> 
+              <div className="cross-icon"></div>
+            </div>
+          </div>
+        </li>
 			)
 		}
 		return content   
@@ -74,18 +83,64 @@ function PickAge({familyMemberState, nextClickHandler})	{
 	},[familyMemberState])
 
 	return (
-		<div>
-  		<Button title="Back" buttonClickHandler={() => nextClickHandler('before_age')}/>
-  		<p>Age</p>
-  		<div style={{"display":"flex"}}>
-  		<Input ref={el => nameRef.current[0] = el}  input={{"type":"text", "value": [familyMemberState.first_name], "placeholder":"Name",
-			      "className":"form-control rounded input-lg text-center no-border"}}/>
-	    <Input ref={el => yearRef.current[0] = el}  input={{"type":"text", "placeholder":"Year",
-	      "className":"form-control rounded input-lg text-center no-border"}}/><span style={{"color":"red"}} ref={el => yearErrRef.current[0] = el}></span>
+		<div className="main-wrapper">
+	    <div className="welcome-screens">
+	      <div className="back-page">
+	        <Back title="Back" buttonClickHandler={() => nextClickHandler('before_age')}/>
 	      </div>
-  		{renderNameAndYear ()}
-  		<Button title="Next" buttonClickHandler={nextHandler}/>
-  	</div>
+	  
+	      <div className="center-content mx-auto welcome-intro">
+	        <div className="content-grid">
+	          <div className="d-flex justify-content-center flex-column">
+	            <h2>
+	              {`${familyMemberState.first_name}, please add first names and year of birth.`}
+	            </h2>
+	            <div className="age-edit-list">
+	              <ol>
+	                <li className="mb-3 ps-2">
+	                  <div className="list-grid">
+	                    <div><Input ref={el => nameRef.current[0] = el}  input={{"type":"text", "value": [familyMemberState.first_name], "placeholder":"Name",
+			      							"className":"form-control"}}/><span className="ms-4">This is you!</span></div>
+	                    <div className="cross-field">
+	                     <Input ref={el => yearRef.current[0] = el}  input={{"type":"text", "placeholder":"Year",
+	      									"className":"form-control"}}/><span style={{"color":"red"}} ref={el => yearErrRef.current[0] = el}></span>
+	                      <div className="cross-icon"></div>
+	                    </div>
+	                  </div>
+	                </li>
+	                {renderNameAndYear ()}
+	              </ol>
+	            </div>
+	            <div className="mt-4">
+	              <Button classes="btn btn-primary" title="Next" buttonClickHandler={nextHandler}/>
+	            </div>
+	          </div>
+	          <div className="d-flex align-items-center">
+	            <img src={require('../../assets/images/welcome_intro_6.svg').default} className="img-fluid" alt="" />
+	          </div>
+	        </div>
+	      </div>
+	  
+	      <div className="footer-links mt-5 px-3">
+	        <div className="d-flex align-items-center">
+	          <a href="./index.html" className="d-flex align-items-center me-4">
+	            <div className="d-flex">
+	              <img src={require('../../assets/images/home-icon.svg').default} className="img-fluid me-1" alt="" />
+	            </div>
+	            Home
+	          </a>
+	          <a href="" className="d-flex align-items-center">
+	            <div className="d-flex">
+	              <img src={require('../../assets/images/info-icon.svg').default} className="img-fluid me-1" alt="" />
+	            </div>
+	            Instructions
+	          </a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+
+		
 	)
 }
 

ca88bb1ea51b9812956d1081544885605ae483c0
joseph jennings <joseph.jennings@gmail.com>
2022-12-20 21:59:02 +0500

    Updated Signup and setup and fix token error on sign up

--- client/src/components/auth/saveBeforeSignup.js
+++ client/src/components/auth/saveBeforeSignup.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import Button from '../UI/button';
+import Back from '../UI/back';
 
-function SaveBeforeSignup({nextClickHandler})	{
+function SaveBeforeSignup({nextClickHandler, familyMemberState: {first_name}})	{
 	return (
-		<div>
-  		<Button title="Back" buttonClickHandler={() => nextClickHandler('confirmAge')}/>
-  		<p>Lets Just save this now</p>
-  		<Button title="Next" buttonClickHandler={() => nextClickHandler('signupSection')}/>
-  	</div>
+		<div className="main-wrapper">
+	    <div className="welcome-screens">
+	      <div className="back-page p-4">
+  				<Back title="Back" buttonClickHandler={() => nextClickHandler('confirmAge')}/>
+	      </div>
+	  
+	      <div className="center-content mx-auto">
+	        <div className="content-grid">
+	          <div className="d-flex justify-content-center flex-column">
+	            <h2>
+	              Nice work, <span className="font-bold">{`${first_name}!`}</span>
+	            </h2>
+	            <h2 className="mt-4">
+	              Okay... before we go on. Let's save your work. Also, by saving - this allows you to stop and finish at a later time.
+	            </h2>
+	            <div className="mt-4">
+	              <Button classes="btn btn-primary" title="Let's Save Our Work" buttonClickHandler={() => nextClickHandler('signupSection')}/>
+	            </div>
+	          </div>
+	          <div>
+	            <img src={require('../../assets/images/welcome_registration.svg').default} className="img-fluid" alt="" />
+	          </div>
+	        </div>
+	      </div>
+	  
+	      <div className="footer-links mt-5 px-3">
+	        <div className="d-flex align-items-center">
+	          <a href="./index.html" className="d-flex align-items-center me-4">
+	            <div className="d-flex">
+	              <img src={require('../../assets/images/home-icon.svg').default} className="img-fluid me-1" alt="" />
+	            </div>
+	            Home
+	          </a>
+	          <a href="" className="d-flex align-items-center">
+	            <div className="d-flex">
+	              <img src={require('../../assets/images/info-icon.svg').default} className="img-fluid me-1" alt="" />
+	            </div>
+	            Instructions
+	          </a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
 	)
 }
 
--- client/src/components/auth/setup.js
+++ client/src/components/auth/setup.js
@@ -3,19 +3,64 @@ import Button from '../UI/button';
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import AuthContext from "../../store/authContext";
 
-function Setup({familyMemberState: {family :{first_name, last_name, email, id, jti, notification}}})	{
+function Setup({familyMemberState: {family :{first_name, last_name, email, id, token, notification}}})	{
 	const ctxAuth = useContext(AuthContext);
 	const navigate = useNavigate();
 	const navigateHandler = (e) => {
 		// navigate(e);
-    ctxAuth.signup({"name": first_name+" "+last_name, "email": email, "id": id, "jti": jti, "notification":notification});
+    ctxAuth.signup({"name": first_name+" "+last_name, "email": email, "id": id, "token": token, "notification":notification});
 
 	}
 	return (
-		<div>
-  		<p>All Setup</p>
-  		<Button title="Next" buttonClickHandler={() => navigateHandler('/')}/>
-  	</div>
+		<div className="main-wrapper">
+	    <div className="welcome-screens">
+	      <div className="back-page p-4">
+	        <a href="./index.html">
+	          <div className="d-flex align-items-center">
+	            <div><img src={require('../../assets/images/back-arrow.svg').default} className="img-fluid me-1" alt="" /></div>
+	            <div className="back-text">Back</div>
+	          </div>
+	        </a>
+	      </div>
+	  
+	      <div className="center-content mx-auto">
+	        <div className="content-grid">
+	          <div className="d-flex justify-content-center flex-column">
+	            <h2 className="m-0">
+	              Yay! You are all set!
+	            </h2>
+	            <h2>
+	              You will now be able to return to where you left off in the survey!
+	            </h2>
+	            <div className="mt-4">
+	              <Button classes="btn btn-primary" title="Continue" buttonClickHandler={() => navigateHandler('/')}/>
+	            </div>
+	          </div>
+	          <div>
+	            <img src={require('../../assets/images/welcome_registration.svg').default} className="img-fluid" alt="" />
+	          </div>
+	        </div>
+	      </div>
+	  
+	      <div className="footer-links mt-5 px-3">
+	        <div className="d-flex align-items-center">
+	          <a href="./index.html" className="d-flex align-items-center me-4">
+	            <div className="d-flex">
+	              <img src={require('../../assets/images/home-icon.svg').default} className="img-fluid me-1" alt="" />
+	            </div>
+	            Home
+	          </a>
+	          <a href="" className="d-flex align-items-center">
+	            <div className="d-flex">
+	              <img src={require('../../assets/images/info-icon.svg').default} className="img-fluid me-1" alt="" />
+	            </div>
+	            Instructions
+	          </a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+
 	)
 }
 

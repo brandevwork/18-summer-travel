50c7090b5c5e5ac506ddeb95a3e626d396a8770e
joseph jennings <joseph.jennings@gmail.com>
2022-12-27 18:15:03 +0500

    Question: Updated the question serializer to send question's image

--- backend/app/serializers/api/v1/questions_controller/questions_serializer.rb
+++ backend/app/serializers/api/v1/questions_controller/questions_serializer.rb
@@ -1,12 +1,17 @@
 module Api::V1::QuestionsController
   class QuestionsSerializer < ActiveModel::Serializer
     include Api::V1::ChoicesController
-    attributes :id, :question_text, :survey_id, :choices
+    include Rails.application.routes.url_helpers
+    attributes :id, :question_text, :survey_id, :question_image, :choices
 
     def choices
       object.choices.map do |choice|
         ChoiceSerializer.new(choice, scope: scope, root: false, question: object)
       end
+    end
+
+    def question_image
+      url_for(object.question_image)
     end
   end
 end

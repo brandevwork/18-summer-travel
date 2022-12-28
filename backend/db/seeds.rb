c6cc94a3a1dca0ec417e04aac48ad55a1321c90c
joseph jennings <joseph.jennings@gmail.com>
2022-12-28 15:27:35 +0500

    Seed: Updated the seed file to store headings against questions

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -11,6 +11,7 @@ survey = Survey.find_or_create_by(survey_name: '18 Summer Tool')
 questions.each_with_index do |question, q_index|
   q = Question.find_or_create_by(question_text: question['question'], survey_id: survey.id)
   q.question_image.attach(io: File.open("app/assets/images/choice_images/q#{q_index + 1}.png"), filename: "q#{q_index + 1}.png")
+  q.update(heading: question['heading']) unless question['heading'].nil?
   question['choices'].each_with_index do |choice, c_index|
     choice = Choice.find_or_create_by(choice_text: choice, question_id: q.id, category_id: question['category_id'])
     choice.image.attach(io: File.open("app/assets/images/choice_images/q#{q_index + 1}_c#{c_index + 1}.jpg"), filename: "q#{q_index + 1}_c#{c_index + 1}.jpg") if q_index < 4

df1c4275ea6f14fb6015b7a4179a1b0cb0744db9
joseph jennings <joseph.jennings@gmail.com>
2022-12-28 15:29:38 +0500

    Handle the scanerio and refactor the code of recommendations API

--- backend/app/controllers/api/v1/recomendations_controller.rb
+++ backend/app/controllers/api/v1/recomendations_controller.rb
@@ -1,19 +1,20 @@
 class  Api::V1::RecomendationsController < BaseController
 
-  swagger_controller :recomendations, "Recomendations"
+  swagger_controller :recomendations, 'Recomendations'
 
   swagger_api :recomendation do
-    summary "Fetches the recomendations of a family"
-    notes "These are the recomendations for a family"
+    summary 'Fetches the recomendations of a family'
+    notes 'These are the recomendations for a family'
     response :unauthorized
-    response :not_acceptable, "The request you made is not acceptable"
+    response :not_acceptable, 'The request you made is not acceptable'
   end
 
   def recomendation
-    all_members = current_family.family_members
-    family_members = all_members.where("age > ?", 4)
+    family_members = current_family.family_members.where('age > ?', 4)
+    min_age = family_members.pluck(:age).min
+    return  render json: { message: 'Recommendations are only given for kids of age less than 18', success: false, status: 204} if min_age > 18
     if family_members.completed.length == family_members.size
-      recomendation_hash = Hash.new
+      recomendation_hash = {}
       family_members.each do |family_member|
         family_member.response_choices.each do |response_choice|
           unless response_choice.choice_id.nil?
@@ -25,16 +26,16 @@ class  Api::V1::RecomendationsController < BaseController
           end
         end
       end
-      sorted_hash = recomendation_hash.sort_by { |_key, value | value }.reverse
-      choice_ids = sorted_hash.map {|sorted_hash| sorted_hash.first }.take(18 - all_members.pluck(:age).min)
+      sorted_hash = recomendation_hash.sort_by { |_key, value| value }.reverse
+      choice_ids = sorted_hash.map {|sorted_hash| sorted_hash.first }.take(18 - min_age)
       choices = []
       choice_ids.each do |choice_id|
         record = Choice.find_by(id: choice_id)
         choices << record
       end
-      render json: { data: choices, success: true, status: 200}
+      render json: { data: choices, success: true, status: 200 }
     else
-      render json: { message: "Survey is not completed by the whole famliy", success: false, status: 204}
+      render json: { message: 'Survey is not completed by the whole famliy', success: false, status: 204 }
     end
   end
 end

65ec84a076245dc1e3f854d1e6c1b7866db6d58b
joseph jennings <joseph.jennings@gmail.com>
2022-12-29 16:03:02 +0500

    Dropdown: Removed 1 from family member number dropdown

--- client/src/components/auth/pickKid.js
+++ client/src/components/auth/pickKid.js
@@ -33,7 +33,6 @@ function PickKid({nextClickHandler}) {
 
 					      <Dropdown.Menu>
 					        <Dropdown.Item eventKey="0">Pick One</Dropdown.Item>
-					        <Dropdown.Item eventKey="1">1</Dropdown.Item>
 					        <Dropdown.Item eventKey="2">2</Dropdown.Item>
 					        <Dropdown.Item eventKey="3">3</Dropdown.Item>
 					        <Dropdown.Item eventKey="4">4</Dropdown.Item>

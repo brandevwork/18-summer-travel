f92fc01ea6cb8ec82bc4c759367165a3d4e4e08e
joseph jennings <joseph.jennings@gmail.com>
2023-01-05 11:54:43 +0500

    Deleted the old architecture models

--- backend/db/migrate/20221206112235_create_responses.rb
+++
@@ -1,9 +0,0 @@
-class CreateResponses < ActiveRecord::Migration[7.0]
-  def change
-    create_table :responses do |t|
-      t.references :family_member, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
--- backend/db/migrate/20221215121719_add_foreign_key_to_choice.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToChoice < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :choices, :category, index: true, foreign_key: true
-  end
-end

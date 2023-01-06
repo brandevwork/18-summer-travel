4e73439de7471f3cafb5b603e168c27765925a79
joseph jennings <joseph.jennings@gmail.com>
2023-01-06 18:35:57 +0500

    Question: Updated the spelling mistake in question model

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   enum question_type: %i[activities destinations]
   has_one_attached :image
   has_many :question_activities
-  has_many :activities, through: :question_actvities
+  has_many :activities, through: :question_activities
   has_many :question_destinations
   has_many :destinations, through: :question_destinations
 end

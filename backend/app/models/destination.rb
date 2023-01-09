d96c1f7616910e01b172bbab65d6818ac19c484f
joseph jennings <joseph.jennings@gmail.com>
2023-01-09 12:47:12 +0500

    Added has_many:through association between destination and activities

--- backend/app/models/destination.rb
+++ backend/app/models/destination.rb
@@ -2,4 +2,6 @@ class Destination < ApplicationRecord
   belongs_to :country
   has_many :question_destinations
   has_many :questions, through: :question_destinations
+  has_many :destination_activities
+  has_many :activities, through: :destination_activities
 end

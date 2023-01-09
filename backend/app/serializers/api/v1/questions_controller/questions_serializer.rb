9a684f4e5ede4f115e8861e49e46c5a68fe6db44
joseph jennings <joseph.jennings@gmail.com>
2023-01-09 12:32:02 +0500

    Choices: Created a new action choices in questions serializer

--- backend/app/serializers/api/v1/questions_controller/questions_serializer.rb
+++ backend/app/serializers/api/v1/questions_controller/questions_serializer.rb
@@ -2,12 +2,15 @@ module Api::V1::QuestionsController
   class QuestionsSerializer < ActiveModel::Serializer
     include Api::V1::ActivitiesController
     include Rails.application.routes.url_helpers
-    attributes :id, :text, :question_image, :heading, :subheading, :boldtext, :activities
-    has_many :destinations
+    attributes :id, :text, :question_image, :heading, :subheading, :boldtext, :question_type, :choices
 
-    def activities
-      object.activities.map do |activity|
-        ActivitySerializer.new(activity, scope: scope, root: false, question: object)
+    def choices
+      if object.destinations.present?
+        object.destinations
+      else
+        object.activities.map do |activity|
+          ActivitySerializer.new(activity, scope: scope, root: false, question: object)
+        end
       end
     end
 

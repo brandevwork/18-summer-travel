9be648707b8033ab4f1bae60a16a4ebf3ae6268f
joseph jennings <joseph.jennings@gmail.com>
2023-01-09 18:42:03 +0500

    Updated the enum question_type

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  enum question_type: %i[activities destinations]
+  enum question_type: %i[activity destination]
   has_one_attached :image
   has_many :question_activities
   has_many :activities, through: :question_activities

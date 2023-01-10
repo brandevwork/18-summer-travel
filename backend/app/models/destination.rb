4ff9c737435feffeb9f884d631048731c7ad9aff
joseph jennings <joseph.jennings@gmail.com>
2023-01-10 13:14:52 +0500

    Removed null true from destination model

--- backend/app/models/destination.rb
+++ backend/app/models/destination.rb
@@ -4,5 +4,5 @@ class Destination < ApplicationRecord
   has_many :questions, through: :question_destinations
   has_many :destination_activities
   has_many :activities, through: :destination_activities
-  has_many :member_preferences, as: :preferenceable, foreign_key: :preferenceable_id
+  has_many :member_preferences, as: :preferenceable
 end

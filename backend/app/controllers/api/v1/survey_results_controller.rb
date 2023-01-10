e4d94c5ffb7bb90f22120d42a4bdd2b428959cb5
joseph jennings <joseph.jennings@gmail.com>
2023-01-10 13:13:35 +0500

    Refactor the code

--- backend/app/controllers/api/v1/survey_results_controller.rb
+++ backend/app/controllers/api/v1/survey_results_controller.rb
@@ -1,21 +1,21 @@
-class  Api::V1::SurveyResultsController < BaseController
+class Api::V1::SurveyResultsController < BaseController
   include Api::V1::FamilyMemberController
 
-  swagger_controller :survey_results, "Survey Result"
+  swagger_controller :survey_results, 'Survey Result'
 
   swagger_api :result do
-    summary "Fetches the survey results of all family members of a family"
-    notes "These are the survey results of all family members of a family"
+    summary 'Fetches the survey results of all family members of a family'
+    notes 'These are the survey results of all family members of a family'
     response :unauthorized
-    response :not_acceptable, "The request you made is not acceptable"
+    response :not_acceptable, 'The request you made is not acceptable'
   end
 
   def result
-    family_members = current_family.family_members.where("age > ?", 4)
+    family_members = current_family.family_members.where('age > ?', 4)
     if family_members.completed.length == family_members.count
       render json: family_members, each_serializer: FamilyMemberSerializer, meta: { status: :ok, code: 200, success: true }
     else
-      render json: { message: "Survey is not completed by the whole famliy", status: 204, success: false}
+      render json: { message: 'Survey is not completed by the whole famliy', status: 204, success: false }
     end
   end
   
